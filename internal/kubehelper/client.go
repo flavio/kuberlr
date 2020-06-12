8c98a5fbf00f3cf796e088ee5af464566044d67b
Flavio Castelli <fcastelli@suse.com>
2020-06-12 18:14:03 +0200

    TIL: you can specify multiple files via KUBECONFIG
    It's possible to specify multiple kubeconfig files via `KUBECONFIG`, for
    example: `KUBECONFIG=~/cluster1.yaml:~/cluster2.yaml`
    See https://github.com/kubernetes/kubernetes/issues/46381#issuecomment-303926031
    
    This commit ensures kuberlr can handle this special case in the same way
    as kubectl does.

--- internal/kubehelper/client.go
+++ internal/kubehelper/client.go
@@ -1,49 +1,47 @@
 package kubehelper
 
 import (
-	"fmt"
-	"os"
-	"path/filepath"
+	"flag"
 	"time"
 
-	"github.com/flavio/kuberlr/internal/common"
 	"k8s.io/client-go/kubernetes"
+	restclient "k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
 func createKubeClient() (*kubernetes.Clientset, error) {
-	kubeconfig := getKubeconfig()
-	if kubeconfig == "" {
-		return nil, fmt.Errorf(
-			"Canot find kubeconfig file. Either create %s or set the KUBECONFIG environment variable\n",
-			defaultKubeconfig())
-	}
+	var cliKubeconfig string
+	flag.StringVar(&cliKubeconfig, "kubeconfig", "", "absolute path to the kubeconfig file")
+	flag.Parse()
 
-	// use the current context in kubeconfig
-	// use the current context in kubeconfig
-	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	var restConfig *restclient.Config
+	var err error
+
+	if cliKubeconfig != "" {
+		// give precedence to --kubeconfig flag
+		restConfig, err = clientcmd.BuildConfigFromFlags("", cliKubeconfig)
+	} else {
+		// Let the NewDefaultClientConfigLoadingRules do the heavy lifting like
+		// parsing the KUBECONFIG value
+		// TIL: it's possible to specify multiple kubeconfig files via KUBECONFIG
+		// For example: `KUBECONFIG=~/cluster1.yaml:~/cluster2.yaml`
+		// See https://github.com/kubernetes/kubernetes/issues/46381#issuecomment-303926031
+		//
+		// The NewDefaultClientConfigLoadingRules function has all the logic built
+		// inside of it that handles this special case.
+		clientConfLoadingrules := clientcmd.NewDefaultClientConfigLoadingRules()
+
+		restConfig, err = clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
+			clientConfLoadingrules,
+			&clientcmd.ConfigOverrides{}).ClientConfig()
+	}
 	if err != nil {
 		return nil, err
 	}
+
 	// lower the timeout value
-	config.Timeout = 1 * time.Second
+	restConfig.Timeout = 1 * time.Second
 
 	// create the clientset
-	return kubernetes.NewForConfig(config)
-}
-
-func getKubeconfig() string {
-	if k := os.Getenv("KUBECONFIG"); k != "" {
-		return k
-	}
-
-	return defaultKubeconfig()
-}
-
-func defaultKubeconfig() string {
-	if home := common.HomeDir(); home != "" {
-		return filepath.Join(home, ".kube", "config")
-	}
-
-	return ""
+	return kubernetes.NewForConfig(restConfig)
 }
